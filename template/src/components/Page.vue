a04f21dc069005bba4a11dae8b44310dab8e0380
Ilia Loginov <i.loginow@outlook.com>
2017-12-07 15:04:19 +0300

    Create svg image of proper size and use it instead of png

--- template/src/components/Page.vue
+++ template/src/components/Page.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="classes">
-    <img src="../images/logo-192.png" alt="logo">
+    <img src="../images/logo-200.svg" alt="logo">
     <h1>\{{ msg }}</h1>
     <router-view/>
     <p>\{{ vuex }}</p>
